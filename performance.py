e0d8133b96b17736262818a5dd82526f2896a3f0
emduc <emilien.duc@gmail.com>
2024-08-17 06:18:05 +0000

    more metrics

--- performance.py
+++ performance.py
@@ -22,17 +22,17 @@ class PerformanceStore:
         self._cv_message_passing_t = 0
         self._cv_grad_reduce_t = 0
         self._local_grad_encryption = []
-        self._comute_loss_times = []
+        self._compute_loss_times = []
         self._back_pass_times = []
         
         
-    def get_comute_loss_times(self):
+    def get_compute_loss_times(self):
         """Get compute loss times"""
-        return self._comute_loss_times
+        return self._compute_loss_times
     
-    def add_comute_loss_time(self, time_t: float):
+    def add_compute_loss_time(self, time_t: float):
         """Add compute loss time"""
-        self._comute_loss_times.append(time_t)
+        self._compute_loss_times.append(time_t)
         
     def get_backward_pass_times(self):
         """Get backward pass times"""
