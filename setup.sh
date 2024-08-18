09a89045d915e3320c97acc9c4ea25f52b2f9bdf
emduc <emilien.duc@gmail.com>
2024-08-18 02:58:16 +0000

    add downloading in setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$1" == "true" ]; then
+    if [ "$#" -ne 3 ]; then
+        echo "Usage: $0 <run_download_script> <world_size> <partition_ID>"
+        exit 1
+    fi
+    run_download_script=true
+    world_size=$2
+    partition_ID=$3
+elif [ "$1" == "false" ]; then
+    if [ "$#" -ne 1 ]; then
+        echo "Usage: $0 <run_download_script>"
+        exit 1
+    fi
+    run_download_script=false
+else
+    echo "Usage: $0 <run_download_script> <world_size> <partition_ID>"
+    exit 1
+fi
+
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 
@@ -10,5 +31,9 @@ pip install --no-deps torchdata==0.7.0
 # Set environment variable (if new machine type, check with ifconfig the if name)
 export GLOO_SOCKET_IFNAME=ens5
 
-# Configure AWS CLI
+# AWS CLI (this is dynamic and asks for access and secret keys)
 aws configure
+
+if [ "$run_download_script" == "true" ]; then
+    python download_dataset.py --s3_dir partitions/$world_size/$partition_ID
+fi
